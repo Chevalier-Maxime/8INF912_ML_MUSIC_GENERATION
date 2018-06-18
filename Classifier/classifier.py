896a61a56a5313049a5cb8f35e847b7c4d22fe10
Chevalier-Maxime <chevalier.maxime@outlook.com>
2018-06-18 17:58:43 -0400

    Improvments on classifier

--- Classifier/classifier.py
+++ Classifier/classifier.py
@@ -9,6 +9,8 @@ import os
 import random
 import numpy as np
 from sklearn import tree
+import shutil
+
 
 import plot_unveil_tree_structure as pretty_path
 #from sklearn.ensemble import RandomForestClassifier
@@ -30,7 +32,7 @@ def exctract_features_from_midi_files(input_folder):
                     os.rename(os.path.join(source,f), os.path.join(dest,f))
                     break
                 except:
-                    print "rename failed, retrying..."
+                    print ("rename failed, retrying...")
            
     if not os.path.isdir(os.path.join(input_folder,'GENERATED','features')):
         os.mkdir(os.path.join(input_folder,'GENERATED','features'))
@@ -59,11 +61,6 @@ def test_features_from_midi_files(args):
     X, _ = map(lambda x: x[0:len(x)-2], vecs), map(lambda x: x[len(x)-2], vecs)
     
 
-    # print(vecs[0])
-    # print(len(vecs[0]))
-
-    # print(X[0])
-    # print(features_index)
     X_new = []
     for full_features in X:
         feature_tmp = []
@@ -71,31 +68,34 @@ def test_features_from_midi_files(args):
             feature_tmp.append(full_features[i])
         X_new.append(feature_tmp)
 
-    # print(X_new[0])
-    # print(X_new)
-    # print(len(X_new[0]))
-    #print(X)
     X_array = np.asarray(X_new)
     y_predicted = model.predict_proba(X_array)
     y_predicted_Label = model.predict(X_array)
 
-    # print(len(y_predicted))
-    # print(X_array[:1])
-    #print("file : %s is classified as : %s" % (vecs[0][len(vecs[0])-1],y_predicted[0]))
+    classes = model.classes_.tolist()
 
+    proba = 0.9
+    if args.proba is not None:
+        proba = args.proba
+    # Resultats
     for i in range(0,len(y_predicted)):
-        print("file : %s is classified as : %s : %s" % (vecs[i][len(vecs[i])-1],y_predicted[i],y_predicted_Label[i]))
+        indexProba = classes.index(y_predicted_Label[i])
+        if y_predicted[i][indexProba] >= proba :
+            filename = vecs[i][len(vecs[i])-1][:-4]
+            print("file : %s is classified as : %s with probability %s (%s)" % (filename, y_predicted_Label[i], y_predicted[i][indexProba], y_predicted[i] ))
+            path_old_file = os.path.join(args.input_folder, 'GENERATED', 'midi', filename)
+            path_new_file = os.path.join(args.output_folder, y_predicted_Label[i])
+            if not os.path.exists(path_new_file):
+                os.makedirs(path_new_file)
+            path_new_file = os.path.join(path_new_file, filename[:-4] + str(y_predicted[i][indexProba]) + '.mid')
+            shutil.copyfile(path_old_file, path_new_file)
+    #Debug option
+    if args.debug_graph:
+        pretty_path.pretty_path(model.estimators_[0])
+        dotfile = open("dt.dot", 'w')
+        tree.export_graphviz(model.estimators_[0], out_file=dotfile)
+        dotfile.close()
     
-    #TODO Debug option
-    # pretty_path.pretty_path(model.estimators_[0])
-    # dotfile = open("dt.dot", 'w')
-    # tree.export_graphviz(model.estimators_[0], out_file=dotfile)
-    # dotfile.close()
-            
-
-
-    
-    # print(y_predicted)
 
 #TODO test if features exists before lunching
 if __name__ == "__main__":
@@ -112,6 +112,10 @@ if __name__ == "__main__":
     # Optional argument for feature extraction
     parser.add_argument('--feature_extraction', action="store_true", help='Does the program\
             need to extract features from midi files (default FALSE)')
+    parser.add_argument('--debug_graph', action="store_true", help='Do you want to output\
+            a graph from the random forest in console and in dt.dot file (default FALSE)')
+    parser.add_argument('--proba', type=float, help='The treshold to validate classification\
+            of a music (default FALSE)')
     
     args = parser.parse_args()
 
