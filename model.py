6642c28eb6dde09052f13e472bf710b4885cf9f4
Kévin Seroux <kevin.seroux@gmail.com>
2018-06-18 10:27:36 -0400

    Entrainement fait

--- model.py
+++ model.py
@@ -5,22 +5,42 @@ import numpy as np
 
 
 def build_model(corpus, max_len, epochs, batch_size):
+    X = []
+    Y = []
+
+    # Split input vector from output
+    for music in corpus:
+        if music is None:
+            continue
+
+        music_length = len(music)
+        for i in range(0, music_length):
+            predictionOffset = i + max_len
+            if predictionOffset > music_length - 1:
+                break
+
+            X.append(music[i:predictionOffset])
+            Y.append(music[predictionOffset])
+
+    X = np.array(X)
+    Y = np.array(Y)
 
     # The in/output size is the same for each sample
     vectorSize = len(corpus[0][0])
 
     # 2 hidden layer LSTM
     model = Sequential()
-    model.add(LSTM(128, return_sequences=True, input_shape=(max_len, vectorSize)))
+    model.add(LSTM(128, return_sequences=True, input_shape=(max_len,
+                                                            vectorSize)))
     model.add(Dropout(0.2))
     model.add(LSTM(128, return_sequences=False))
     model.add(Dropout(0.2))
     model.add(Dense(vectorSize))
     model.add(Activation('softmax'))
 
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop',
+                  metrics=['accuracy'])
 
-    # le model.fit permet d'entrainer le model, sauf que je sais pas vraiment comment lui dire de s'entrainer sur tel ou tel trucs
-    # model.fit(<<<TESTS SONGS>>>, batch_size=128, nb_epoch=N_epochs)
+    model.fit(X, Y, batch_size=batch_size, nb_epoch=epochs)
 
     return model
